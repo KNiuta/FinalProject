d3d0ddefeb8e6b2469b34e073228a17e4b53601d
Niuta <Niuta_87@mail.ru>
2023-04-09 21:16:36 +0600

    Metod StringRand

---
+++ Example/Program.cs
@@ -0,0 +1,19 @@
+﻿
+/// <summary>
+/// Метод формерует строку из случайных букв заданной длинны 
+/// </summary>
+/// <param name="length"> длинна строки </param>
+/// <returns>вывод рандомной стороки </returns>
+string StringRand(int length)
+{
+              int randValue;
+              string str = "";
+              char letter;
+              for (int i = 0; i < length; i++)
+              {
+                            randValue = new Random().Next(0, 26);
+                            letter = Convert.ToChar(randValue + 65);
+                            str = str + letter;
+              }
+              return str;
+}
